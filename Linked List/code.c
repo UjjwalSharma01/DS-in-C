41ac74add9ccbedecbd429f6b8afc8d140a6ca45
UjjwalSharma01 <ujjwalsh2004@gmail.com>
2023-10-05 22:18:15 +0530

    made some changes

--- Linked List/code.c
+++ Linked List/code.c
@@ -7,28 +7,68 @@ typedef struct node{
 } node;
 
 int main(){
-    node *head = NULL,*newnode,*temp;
-    int n=1;
-    while(n!=0){
-    newnode = malloc(sizeof(node));
-    printf("Enter the value of the node: ");
+    // node *head = NULL,*newnode,*temp;
+    // int n=1;
+    // while(n!=0){
+    // newnode = malloc(sizeof(node));
+    // printf("Enter the value of the node: ");
+    // scanf("%d",&newnode->value);
+    // newnode->next = NULL;
+    // if(head == NULL){
+    // head = newnode;
+    // }
+    // else{
+    //     temp = head;
+    //     temp->next = newnode;
+    // }
+    // printf("You want to add one more value to the linkedlist ? ");
+    // scanf("%d", &n);
+    
+    // }
+    // // Printing the linked list
+    // temp = head;
+    // while(temp!=NULL){
+    //     printf("%d ",temp->value);
+    //     temp = temp->next;
+    // }
+
+
+
+    // Insertion at the beginning in the Linked list
+    node *head =NULL, *temp, *newnode, *temp2;
+    newnode = (node*)malloc(sizeof(node));
+    // entering one node in adv
+    printf("Enter the Value of the first node: ");
     scanf("%d",&newnode->value);
-    newnode->next = NULL;
+    newnode->next = NULL; 
     if(head == NULL){
-    head = newnode;
+        head = newnode;   
     }
     else{
         temp = head;
         temp->next = newnode;
+        free(temp);
     }
-    printf("You want to add one more value to the linkedlist ? ");
-    scanf("%d", &n);
-    
-    }
-    // Printing the linked list
+
+    // Printing
+    temp = head;
+    while(temp != NULL){
+    printf("The value you entered is: \n");
+    printf("%d", temp->value);
+    temp = temp->next;
+}
+
+    // // adding new node
+    // newnode->value = 99;
+
+    // temp2 = head;
+    // head = newnode;
+    // head->next = temp2;
+
+    // printing the updated linked List
     temp = head;
     while(temp!=NULL){
-        printf("%d ",temp->value);
+        printf("%d",temp->value);
         temp = temp->next;
     }
     return 0;
